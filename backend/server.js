6ecd6c4d8f17f375c16e896b17e32768407ae51e
Devaadharshan <devaadharshan127@gmail.com>
2026-02-05 09:25:58 +0530

    Database Connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+dotenv.config();
+connectDB();
+
+const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend is running");
+  res.send("Backend running with DB connected");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
